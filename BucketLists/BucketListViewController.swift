e59ea7aec6289e7516b2058272a5ab9ca2a39471
Kevin Kibet <kevinkibetw7@gmail.com>
2021-10-11 15:00:12 +0300

    - added core data to project - implemented save and retrieve methods in core data

--- BucketLists/BucketListViewController.swift
+++ BucketLists/BucketListViewController.swift
@@ -6,17 +6,28 @@
 //
 
 import UIKit
+import CoreData
 
 class BucketListViewController: UITableViewController {
     
     
     var items = ["Book", "laptop", "Pen", "bag"]
     
+    var students = [Student]()
+    
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Bucketlist"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addItem))
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchAllItems()
+
     }
     
     
@@ -27,14 +38,33 @@ class BucketListViewController: UITableViewController {
         self.navigationController?.pushViewController(addItemVC, animated: true)
     }
     
+    func fetchAllItems() {
+        let itemRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        
+        do {
+            let results = try managedObjectContext.fetch(itemRequest)
+            students = results as! [Student]
+            
+            for student in students {
+                print("\(student.name)")
+            }
+            
+        } catch {
+            print("\(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return students.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "mycell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = students[indexPath.row].name
         return cell
         
     }
@@ -44,9 +74,21 @@ class BucketListViewController: UITableViewController {
 extension BucketListViewController: SaveItemDelegate {
     func saveItemText(_ item: String) {
         
-//        let addItem = storyboard?.instantiateViewController(withIdentifier: "add") as! AddItemTableViewController
-        items.append(item)
-        tableView.reloadData()
+        let student = NSEntityDescription.insertNewObject(forEntityName: "Student", into: managedObjectContext) as! Student
+        student.name = item
+        
+        guard let myStudent = student.name else { return }
+        
+        if managedObjectContext.hasChanges {
+            do {
+                try managedObjectContext.save()
+                print("Success", myStudent)
+            } catch {
+                print("\(error)")
+            }
+        }
+        
+    
     }
     
     
